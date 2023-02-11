023be496fb092b162a7963573dac49a555d6f879
mdhamidulislamT <hamidlive6@gmail.com>
2023-02-11 13:09:55 +0600

    added search done

--- front-end/src/products/ProductList.js
+++ front-end/src/products/ProductList.js
@@ -8,22 +8,34 @@ import Table from 'react-bootstrap/Table';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import { ToastContainer, toast } from 'react-toastify';
+import Form from "react-bootstrap/Form";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+
+
     const notify = (msg) => toast.success(msg, {
       theme: "colored"
     })
     const notifyEror = (msg) => toast.error(msg, {
       theme: "colored"
     })
-    
 
     
+  useEffect(() => {
+      getProducts();
+  }, []);
 
   useEffect(() => {
-    getProducts();
-  }, []);
+    const delayDebounceFn = setTimeout(() => {
+      // Send fetch request here
+      searchProducts(searchTerm)
+    }, 2000)
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm])
+
 
   const getProducts = async () => {
     let result = await fetch("http://localhost:3003/products");
@@ -48,58 +60,83 @@ const ProductList = () => {
     });
   };
 
-  
+  // Search Products
+  const searchProducts = async (searchTerm)=>{
+    //let key = event.target.value;
+    let key = searchTerm;
+
+    if (key) {
+      let result = await fetch(`http://localhost:3003/search/${key}`);
+      result = await result.json();
+      
+      if (result) {
+        setProducts(result.data);
+      }else{
+        notifyEror("Error! Please try again.");
+      }
+
+    }else{
+      getProducts();
+    }
+
+  }
 
   return (
     <Container>
       <Row>
         <Hero title="Product List" />
 
+        <Col md={6} className="mt-3">
+            <Form.Group className="mb-3" controlId="searchProduct">
+            <Form.Control type="text" placeholder="Search Your Products..." onChange={(e) => setSearchTerm(e.target.value)} />
+          </Form.Group>
+        </Col>
+        <Col md={{ span: 2, offset: 4 }} className="mt-3">
+        <Button variant="success" onClick={getProducts}>Refresh</Button>
+        </Col>
+
         <Col md={12}>
           {/* <ProductTable products={products} /> */}
           <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Category</th>
-          <th>Price</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
 
-            {
-                products ?
-                
-                Object.entries(products).map(([key, prodduct])=>(
-                
-                    <tr key={key}>
-                        <td>{(parseInt(key)+1)}</td>
-                        <td>{prodduct.name}</td>
-                        <td>{prodduct.category}</td>
-                        <td>{prodduct.price}</td>
-                        <td>
-                            <Link  to={"/edit/"+prodduct._id}>  Edit </Link>
-                          <Button variant="danger"  onClick={() => deleteProduct(prodduct._id)}>
-                            Delete
-                          </Button>
-                        </td>
-                    </tr>
-                 
-                ))
-                : 
-                <tr>
-                    <td colSpan={4}><h1> Data Loading... </h1></td>
-                </tr>
-            }
-                
-               
-                
-            
+                  {
+                      products ?
+                      
+                      Object.entries(products).map(([key, prodduct])=>(
+                      
+                          <tr key={key}>
+                              <td>{(parseInt(key)+1)}</td>
+                              <td>{prodduct.name}</td>
+                              <td>{prodduct.category}</td>
+                              <td>{prodduct.price}</td>
+                              <td>
+                                  <Link  to={"/edit/"+prodduct._id}>  Edit </Link>
+                                <Button variant="danger"  onClick={() => deleteProduct(prodduct._id)}>
+                                  Delete
+                                </Button>
+                              </td>
+                          </tr>
+                      
+                      ))
+                      : 
+                      <tr>
+                          <td colSpan={4}><h1> Data Loading... </h1></td>
+                      </tr>
+                  }
+                      
 
-      </tbody>
-    </Table>
+            </tbody>
+          </Table>
         </Col>
       </Row>
       <ToastContainer />
